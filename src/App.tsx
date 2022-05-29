7ba617ffbf5a6bf0167209ce4935abece56b3f73
Andrii Klochko <andrii.k@aquimarketing.com>
2022-05-29 23:41:07 +0300

    Make page decorations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from 'react'
+
+import PageBackgroundDecoration from '@components/PageBackgroundDecoration'
 
 const App = () => {
-    return <div>Hello world</div>
+    return (
+        <>
+            <div className="page__wrapper">{/* Other page content will be here */}</div>
+            <PageBackgroundDecoration />
+        </>
+    )
 }
 
 export default App
