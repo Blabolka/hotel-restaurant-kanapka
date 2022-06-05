b503dabcdb5e58d703c73a7825c7ae8e123e3dc1
Andrii Klochko <andrii.k@aquimarketing.com>
2022-06-05 14:24:35 +0300

    Implement grid with elements

---
+++ src/pages/MainPage/CenterBlock/DishItemsGrid/DishItemsGrid.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import { Stack } from '@mui/material'
+import DishBlockItem from '@components/Dishes/DishBlockItem'
+
+const MOCKS = [
+    {
+        imagePath: 'test',
+        name: 'Курячі шашлички',
+        description: '5 шматочків курячої грудки маринують і приправляють 24 години, обсмажені на шампурі.',
+        weight: 450,
+        price: 230,
+    },
+    {
+        imagePath: 'test',
+        name: 'Салат зі смаженими кальмарами',
+        description: '5 шматочків курячої грудки маринують і приправляють 24 години, обсмажені на шампурі.',
+        weight: 450,
+        price: 230,
+    },
+    {
+        imagePath: 'test',
+        name: 'Курячі шашлички',
+        description: '5 шматочків курячої грудки маринують і приправляють 24 години, обсмажені на шампурі.',
+        weight: 450,
+        price: 230,
+    },
+    {
+        imagePath: 'test',
+        name: 'Курячі шашлички',
+        description: '5 шматочків курячої грудки маринують і приправляють 24 години, обсмажені на шампурі.',
+        weight: 450,
+        price: 230,
+    },
+    {
+        imagePath: 'test',
+        name: 'Курячі шашлички',
+        description: '5 шматочків курячої грудки маринують і приправляють 24 години, обсмажені на шампурі.',
+        weight: 450,
+        price: 230,
+    },
+    {
+        imagePath: 'test',
+        name: 'Курячі шашлички',
+        description: '5 шматочків курячої грудки маринують і приправляють 24 години, обсмажені на шампурі.',
+        weight: 450,
+        price: 230,
+    },
+]
+
+const DishItemsGrid = () => {
+    return (
+        <Stack direction="row" flexWrap="wrap" gap="20px">
+            {MOCKS.map((item, index) => {
+                return <DishBlockItem key={index} dishInfo={item} />
+            })}
+        </Stack>
+    )
+}
+
+export default DishItemsGrid
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import CenterBlock from './CenterBlock/MainPageCenterBlock'
+
+const MainPage = () => {
+    return <CenterBlock />
+}
+
+export default MainPage
