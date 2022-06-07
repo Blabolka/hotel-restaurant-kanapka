437127a11f1b1c3a54efbb18041fa5781d5a4c06
Andrii Klochko <andrii.k@aquimarketing.com>
2022-06-07 21:28:23 +0300

    Remove comment for debounce hook

--- src/utils/hooks/useDebounce.ts
+++ src/utils/hooks/useDebounce.ts
@@ -4,12 +4,10 @@ export const useDebounce = (value, delay) => {
     const [debouncedValue, setDebouncedValue] = useState(value)
 
     useEffect(() => {
-        // Update debounced value after delay
         const handler = setTimeout(() => {
             setDebouncedValue(value)
         }, delay)
 
-        // Clear timeout after component unmount and setTimeout will not fire
         return () => {
             clearTimeout(handler)
         }
