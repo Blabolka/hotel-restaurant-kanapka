946b805060451c4e6db72d9e286b12048cbe8e4d
Andrii Klochko <andrii.k@aquimarketing.com>
2022-06-07 15:38:35 +0300

    Fixes after comments

--- src/pages/MainPage/CenterBlock/Filtering/DishSortingSelect/dishSortingSelectUtils.ts
+++ src/pages/MainPage/CenterBlock/Filtering/DishSortingSelect/dishSortingSelectUtils.ts
@@ -2,3 +2,14 @@ export enum SORTING_SELECT_VALUES {
     ASC = 'asc',
     DESC = 'desc',
 }
+
+export const sortingMenuItems = [
+    {
+        value: SORTING_SELECT_VALUES.ASC,
+        name: 'Від дешевих до дорогих',
+    },
+    {
+        value: SORTING_SELECT_VALUES.DESC,
+        name: 'Від дорогих до дешевих',
+    },
+]
