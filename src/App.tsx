718a9c06c864d15ee7b265ebb2e2e640671bb716
Romanov Nikita <romunoff@gmail.com>
2022-06-08 19:28:10 +0300

    Minor style fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,30 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Loading from '@components/Loading'
 import PageBackgroundDecoration from '@components/PageBackgroundDecoration'
+import { createTheme, ThemeProvider } from '@mui/material'
 
 const MainPage = React.lazy(() => import('@pages/MainPage/MainPage'))
 
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Rubik',
+    },
+})
+
 const App = () => {
     return (
-        <BrowserRouter>
-            <div className="page__wrapper">
-                <React.Suspense fallback={<Loading />}>
-                    <Routes>
-                        <Route path="/" element={<MainPage />} />
-                    </Routes>
-                </React.Suspense>
-            </div>
-            <PageBackgroundDecoration />
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <div className="page__wrapper">
+                    <React.Suspense fallback={<Loading />}>
+                        <Routes>
+                            <Route path="/" element={<MainPage />} />
+                        </Routes>
+                    </React.Suspense>
+                </div>
+                <PageBackgroundDecoration />
+            </BrowserRouter>
+        </ThemeProvider>
     )
 }
 
