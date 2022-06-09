23a28efd33238015a0c4d96e03799822d962e724
Andrii Klochko <andrii.k@aquimarketing.com>
2022-06-09 22:29:58 +0300

    Start implement delivery block

---
+++ src/pages/MainPage/RightBlock/Delivery/Delivery.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Stack } from '@mui/material'
+
+export default function Delivery() {
+    return (
+        <Stack>
+            <span className="font-size-18px color-secondary">Доставка</span>
+        </Stack>
+    )
+}
