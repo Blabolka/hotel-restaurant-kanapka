045cbb72b695092ba85990cc8ff0485770bb1c80
Romanov Nikita <romunoff@gmail.com>
2022-06-11 15:54:47 +0300

    Implement global custom table and admin menu design

--- src/components/Dishes/dishItemUtils.ts
+++ src/components/Dishes/dishItemUtils.ts
@@ -5,6 +5,7 @@ export interface DishInfo {
     description: string
     weight: number
     price: number
+    dishType: string
 }
 
 export interface DishCartInfo extends DishInfo {
--- src/pages/AdminPage/CenterBlock/CenterBlock.tsx
+++ src/pages/AdminPage/CenterBlock/CenterBlock.tsx
@@ -1,19 +1,168 @@
-import React from 'react'
-import { Stack } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { Box, Button, TextField, Typography } from '@mui/material'
 import DishSearchBar from '@pages/MainPage/CenterBlock/Filtering/DishSearchBar/DishSearchBar'
 import DishSortingSelect from '@pages/MainPage/CenterBlock/Filtering/DishSortingSelect/DishSortingSelect'
-import FormRow from '@pages/AdminPage/FormRow/FormRow'
+import { createStyles, makeStyles } from '@mui/styles'
+import CustomTable, { Column, Data, DataValueTypes } from '@pages/AdminPage/CustomTable/CustomTable'
+import { getColumns, getRows } from '@pages/AdminPage/CenterBlock/centerBlockUtils'
+import { getDishesAsync } from '@redux-actions/mainPageActions'
+import { DishInfo } from '@components/Dishes/dishItemUtils'
+import { useAppDispatch, useAppSelector } from '@hooks'
+import edit from '@assets/img/edit.svg'
+import remove from '@assets/img/remove.svg'
+import save from '@assets/img/save.svg'
+import undo from '@assets/img/undo.svg'
+import picture from '@assets/img/picture.svg'
+import CloseIcon from '@mui/icons-material/Close'
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined'
 
 export default function CenterBlock() {
+    const classes = useStyles()
+    const dispatch = useAppDispatch()
+
+    const dishes: DishInfo[] = useAppSelector((state) => state.mainPage.dishes)
+
+    const [editRowIds, setEditRowIds] = useState<(string | number)[]>([])
+
+    useEffect(() => {
+        dispatch(getDishesAsync())
+    }, [])
+
+    const formatTextFieldColumn = (value: DataValueTypes, row: Data, columnName: string) => {
+        return (
+            <TextField
+                className={classes.textField}
+                placeholder={`${columnName}...`}
+                defaultValue={value}
+                onBlur={(event) => console.log(event.target.value)}
+                disabled={!editRowIds.includes(row.id)}
+                multiline
+                fullWidth
+            />
+        )
+    }
+
+    const formatPhotoColumn = (value: DataValueTypes, row: Data) => {
+        const filename = value?.toString().split('/').pop()
+        return filename ? (
+            <Box className={classes.fileContainer}>
+                <Box className={classes.loadedFile}>
+                    <img src={picture} alt="Picture" />
+                    <Typography variant="body2">{filename}</Typography>
+                </Box>
+                {editRowIds.includes(row.id) && <CloseIcon onClick={() => {}} fontSize="small" />}
+            </Box>
+        ) : (
+            <Button className={classes.downloadButton} component="label">
+                <Typography variant="body2">Завантажити</Typography>
+                <FileDownloadOutlinedIcon fontSize="small" />
+                <input type="file" hidden onChange={() => {}} />
+            </Button>
+        )
+    }
+
+    const createActions = (value: DataValueTypes, row: Data) => {
+        return editRowIds.includes(row.id) ? (
+            <Box className={classes.actionButtons}>
+                <img
+                    src={save}
+                    alt="Save"
+                    onClick={() => {
+                        setEditRowIds(editRowIds.filter((item: string | number) => item !== row.id))
+                    }}
+                />
+                <img
+                    src={undo}
+                    alt="Undo"
+                    onClick={() => setEditRowIds(editRowIds.filter((item: string | number) => item !== row.id))}
+                />
+            </Box>
+        ) : (
+            <Box className={classes.actionButtons}>
+                <img src={edit} alt="Edit" onClick={() => setEditRowIds([...editRowIds, row.id])} />
+                <img src={remove} alt="Remove" />
+            </Box>
+        )
+    }
+
+    const columns: Column[] = getColumns(formatTextFieldColumn, formatPhotoColumn, createActions)
+    const rows: Data[] = getRows(dishes)
+
     return (
-        <Stack direction="column" gap="30px" alignItems="center">
-            <Stack direction="column" gap="20px" minWidth={760}>
-                <Stack direction="row" gap="20px">
+        <Box className={classes.root}>
+            <Box className={classes.container}>
+                <Box className={classes.filtersContainer}>
                     <DishSearchBar />
                     <DishSortingSelect />
-                </Stack>
-                <FormRow />
-            </Stack>
-        </Stack>
+                </Box>
+                <CustomTable columns={columns} rows={rows} />
+            </Box>
+        </Box>
     )
 }
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        root: {
+            width: '100%',
+        },
+        container: {
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        filtersContainer: {
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '20px',
+            marginBottom: '20px',
+        },
+        textField: {
+            '& .MuiOutlinedInput-root': {
+                padding: 0,
+                fontSize: '14px',
+                fontWeight: 400,
+                '& input': {
+                    padding: 0,
+                },
+                '& fieldset': {
+                    border: 'none',
+                },
+                '&.Mui-focused fieldset': {
+                    border: '0',
+                },
+            },
+        },
+        downloadButton: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            gap: '10px',
+            color: 'black',
+            padding: 0,
+            textTransform: 'none',
+        },
+        fileContainer: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+        },
+        loadedFile: {
+            display: 'flex',
+            alignItems: 'center',
+            gap: '10px',
+            '& .MuiTypography-root': {
+                width: '90px',
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
+            },
+        },
+        actionButtons: {
+            display: 'flex',
+            gap: '20px',
+            '& img:hover': {
+                cursor: 'pointer',
+            },
+        },
+    }),
+)
